db36f095496ca95c405686e73b009ba074afe3cf
wyrobowa <aniawyroba@gmail.com>
2026-02-27 08:45:17 +0100

    chore: update gh pages action

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || './',
+  base: '/test-react-redux-app/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
